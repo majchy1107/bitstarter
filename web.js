f3011b93af4acda6ea4db02cd29d3618636f2202
maja <maja@thinky.(none)>
2013-07-11 13:00:19 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = new Buffer(100);
+
+buffer = fs.readFileSync(index.html);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toStr());
 });
 
 var port = process.env.PORT || 5000;
